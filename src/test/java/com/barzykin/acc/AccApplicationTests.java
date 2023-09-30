a76068d3b456b809bbeed5158d27842598fc7929
Alexej Barzykin <alexej.barzykin@gmail.com>
2023-09-30 16:44:14 +0400

    + simple test if context loads

--- src/test/java/com/barzykin/acc/AccApplicationTests.java
+++ src/test/java/com/barzykin/acc/AccApplicationTests.java
@@ -1,13 +1,20 @@
 package com.barzykin.acc;
 
+import com.barzykin.acc.endpoints.CalcEndpoint;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class AccApplicationTests {
+    @Autowired
+    private CalcEndpoint calcEndpoint;
 
     @Test
     void contextLoads() {
+        assertNotNull(calcEndpoint);
     }
 
 }
